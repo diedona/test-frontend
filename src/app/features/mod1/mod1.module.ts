30b111fad5070d22492035249956971b6228f745
diedona <diedona@gmail.com>
2024-09-05 19:09:57 -0300

    removing components

--- src/app/features/mod1/mod1.module.ts
+++ src/app/features/mod1/mod1.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Mod1RoutingModule } from './mod1-routing.module';
-import { Mod1Component } from './mod1.component';
 import { HelloMod1Component } from './pages/hello-mod1/hello-mod1.component';
 
 
 @NgModule({
   declarations: [
-    Mod1Component,
     HelloMod1Component
   ],
   imports: [
